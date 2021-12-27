610506553a611eee85e7598f591abbb2bb233422
kartikdhomne <92445043+kartikdhomne@users.noreply.github.com>
2021-12-28 00:00:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">Simple Calculator</h1>
+    <p data-ns-test="project-description"> This simple calculator does all basic mathmatical calculation </p>
+    
+    </div>
   )
 }
 
