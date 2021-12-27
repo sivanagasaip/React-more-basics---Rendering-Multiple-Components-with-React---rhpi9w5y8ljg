88aed68d1cb876d42e9c811ee375dfdf29807902
kartikdhomne <92445043+kartikdhomne@users.noreply.github.com>
2021-12-28 00:16:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,16 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    <h1 data-ns-test="project-name">Simple Calculator</h1>
-    <p data-ns-test="project-description"> This simple calculator does all basic mathmatical calculation </p>
+    <h1 data-ns-test="project-name">Simple Calculator</h1>;
+    <p data-ns-test="project-description"> This simple calculator does all basic mathmatical calculation </p>;
     
     </div>
   )
+  
+ReactDOM.render(
+  App,
+  document.getElementById('root')
+);
 }
 
 
